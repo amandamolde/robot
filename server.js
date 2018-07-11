a103b36eb1cd4f2abccae8aa6d2677423926fdc9
Amanda Molde <amanda.molde@gmail.com>
2018-07-11 12:17:15 -0600

    Created an index route to display objects, installed ejs

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
-const express = require(express);
+const express = require('express');
 const app = express();
+
+// require our model
+
+const Robots = require('./models/robots');
+
+// Index Route
+app.get('/robots', (req, res) => {
+	res.render('index.ejs', {
+		robots: Robots
+	});
+});
+
+app.listen(3000, () => {
+	console.log('I am listening on port 3000');
+});
