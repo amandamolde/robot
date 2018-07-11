67088c04a4f0bd6a009b0e02aa2f464b1bc709fa
Amanda Molde <amanda.molde@gmail.com>
2018-07-11 12:30:04 -0600

    Installed method-override, created a DELETE route handler

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
 
 // require our model
 
@@ -10,6 +14,11 @@ app.get('/robots', (req, res) => {
 	res.render('index.ejs', {
 		robots: Robots
 	});
+});
+
+app.delete('/robots/:index', (req, res) => {
+	Robots.splice(req.params.index, 1);
+	res.redirect('/robots');
 });
 
 app.listen(3000, () => {
