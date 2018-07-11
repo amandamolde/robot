ae4546fb4947742adce807bb9da49f1355f0a551
Amanda Molde <amanda.molde@gmail.com>
2018-07-11 12:07:23 -0600

    Created an array of my CRUDable objects

---
+++ models/robots.js
@@ -0,0 +1,22 @@
+const robots = [
+	{
+		name: 'C-3PO',
+		homeworld: 'Tatooine',
+		affiliation: 'Skywalker family',
+		color: 'golden',
+	},
+	{
+		name: 'R2-D2',
+		homeworld: 'Naboo',
+		affiliation: 'Jedi Order',
+		color: 'blue and silver',
+	},
+	{
+		name: 'BB-8',
+		homeworld: 'Yavin 4',
+		affiliation: 'Resistance',
+		color: 'white, grey, and orange',
+	},
+];
+
+module.exports = robots;
